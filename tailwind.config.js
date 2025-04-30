f23b6c1c68d154a5b1374c01eebc69159f20e2c0
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-04-30 15:22:51 +0200

    Add tailwind-custom-data.json file to .vscode directory.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -12,4 +13,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
