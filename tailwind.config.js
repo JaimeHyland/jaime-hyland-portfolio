09f599cf68244ef564087d3186d328ad8c36c280
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-04-30 14:18:02 +0200

    Repair tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -17,4 +12,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
