50b8f37a0a0de6538f09d9db95956560b7a90cbf
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-05-05 15:55:35 +0200

    Modify content paths to match src/app/ structure.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", // optional, if you have components here
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",      // optional, if you use /pages
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",            // broad fallback
   ],
   safelist: [
     'text-2xl',
