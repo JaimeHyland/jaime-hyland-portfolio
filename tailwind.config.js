0c7fc22c0529ee5849c9550189399df2b2ff7152
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-06-09 12:25:25 +0200

    Convert from CommonJS to ES modules.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  "./app/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
