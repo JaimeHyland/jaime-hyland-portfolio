087e815764242cc5271d1a2c147612684f13891d
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-07-07 11:08:18 +0200

    Resolve readonly home error.

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import { localizedLabels } from '@/lib/labels';
 
 interface HeaderProps {
   lang: 'en' | 'es' | 'de';
-  labels: typeof localizedLabels['en'];
+  labels: (typeof localizedLabels)[keyof typeof localizedLabels];
   paths: {
     home: string;
     projects: string;
