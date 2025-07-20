fd6f76d1130790d3e9a8d71c659ff7478da048d4
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-07-20 07:45:18 +0200

    Add technologies to homepage, put them in some order.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,9 +5,10 @@ import { getLocale } from 'next-intl/server';
 
 import {
   SiPython, SiDjango, SiPostgresql, SiMysql, SiGithub, SiGodaddy, SiHeroku,
-  SiVercel, SiNextdotjs, SiTailwindcss, SiStripe, SiAmazonaws,
+  SiVercel, SiNextdotjs, SiTypescript, SiTailwindcss, SiStripe, SiAmazonaws,
   SiBootstrap, SiFlask, SiJira, SiJupyter, SiHtml5, SiCss3, SiDart,
-  SiFlutter, SiSwagger, SiDotnet,
+  SiFlutter, SiSwagger, SiDotnet, SiNodedotjs, SiReact, SiVisualstudiocode,
+  SiCplusplus, SiVisualstudio, SiAndroidstudio, SiEclipseide, SiGit
 } from 'react-icons/si';
 import { FaJava } from 'react-icons/fa';
 import LearningSection from '@/components/LearningSection';
@@ -55,36 +56,58 @@ export default async function HomePage() {
       </section>
 
 
-      {/* Tech Stack */}
       <section className="mb-16">
+        
         <h2 className="text-2xl font-semibold text-center mb-8">{techsTitle}</h2>
         <div className="grid grid-cols-3 sm:grid-cols-5 gap-6 justify-items-center text-gray-800">
+          {/* Languages */}
           <SiPython className="w-10 h-10" title="Python" />
+          <SiTypescript className="w-10 h-10" title="TypeScript" />
+          <SiDart className="w-10 h-10" title="Dart" />
+          <FaJava className="w-10 h-10" title="Java Swing" />
+          <SiCplusplus className="w-10 h-10" title="C++" />
+          <SiDotnet className="w-10 h-10" title="Visual Basic .NET" />
+          <SiHtml5 className="w-10 h-10" title="HTML5" />
+          <SiCss3 className="w-10 h-10" title="CSS3" />
+
+          {/* Frameworks & Libraries */}
           <SiDjango className="w-10 h-10" title="Django" />
+          <SiFlask className="w-10 h-10" title="Flask" />
+          <SiReact className="w-10 h-10" title="React" />
+          <SiNextdotjs className="w-10 h-10" title="Next.js" />
+          <SiFlutter className="w-10 h-10" title="Flutter" />
+          <SiBootstrap className="w-10 h-10" title="Bootstrap" />
+          <SiTailwindcss className="w-10 h-10" title="Tailwind CSS" />
+
+          {/* Databases */}
           <SiPostgresql className="w-10 h-10" title="PostgreSQL" />
           <SiMysql className="w-10 h-10" title="MySQL" />
-          <SiGithub className="w-10 h-10" title="GitHub" />
-          <SiGodaddy className="w-10 h-10" title="GoDaddy" />
+
+           {/* Deployment / Hosting */}
           <SiHeroku className="w-10 h-10" title="Heroku" />
           <SiVercel className="w-10 h-10" title="Vercel" />
-          <SiNextdotjs className="w-10 h-10" title="Next.js" />
-          <SiTailwindcss className="w-10 h-10" title="Tailwind CSS" />
-          <SiStripe className="w-10 h-10" title="Stripe" />
+          <SiGodaddy className="w-10 h-10" title="GoDaddy" />
           <SiAmazonaws className="w-10 h-10" title="AWS" />
           <div className="flex items-center justify-center w-10 h-10">
             <img src="/images/cloudinary_cloud.png" alt="Cloudinary" className="max-h-full max-w-full object-contain" title="Cloudinary" />
           </div>
-          <SiBootstrap className="w-10 h-10" title="Bootstrap" />
-          <SiFlask className="w-10 h-10" title="Flask" />
+
+          {/* Payments and APIs */}
+          <SiStripe className="w-10 h-10" title="Stripe" />
+          <SiSwagger className="w-10 h-10" title="Swagger" />
+
+          {/* Tools, IDEs & Dev Workflow */}
+          <SiVisualstudiocode className="w-10 h-10" title="Visual Studio Code" />
+          <SiVisualstudio className="w-10 h-10" title="Visual Studio" />
+          <SiAndroidstudio className="w-10 h-10" title="Android Studio" />
+          <SiEclipseide className="w-10 h-10" title="Eclipse" />
+          <SiGithub className="w-10 h-10" title="GitHub" />
+          <SiGit className="w-10 h-10" title="Git" />
           <SiJira className="w-10 h-10" title="Jira" />
           <SiJupyter className="w-10 h-10" title="Jupyter" />
-          <SiHtml5 className="w-10 h-10" title="HTML5" />
-          <SiCss3 className="w-10 h-10" title="CSS3" />
-          <FaJava className="w-10 h-10" title="Java Swing" />
-          <SiDart className="w-10 h-10" title="Dart" />
-          <SiFlutter className="w-10 h-10" title="Flutter" />
-          <SiSwagger className="w-10 h-10" title="Swagger" />
-          <SiDotnet className="w-10 h-10" title="Visual Basic .NET" />
+          
+          {/* Runtime Environment */}
+          <SiNodedotjs className="w-10 h-10" title="Node.js" />
         </div>
       </section>
 
