baf03aa26d81350d95a611b53c93269a3729540e
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-10-29 10:52:02 +0100

    Add robots.ts and sitemap.ts, generatl robots.txt, sitemap.xml.

--- app/i18n/request.ts
+++ app/i18n/request.ts
@@ -1,16 +1,22 @@
 import { getRequestConfig } from "next-intl/server";
 
+const DEFAULT_LOCALE = "en";
+
 export default getRequestConfig(async (params) => {
   const locale = await params.requestLocale;
 
-  if (!locale) {
-    throw new Error("Locale is undefined in getRequestConfig.");
+if (!locale) {
+    return {
+      locale: DEFAULT_LOCALE,
+      messages: {}
+    };
   }
 
   const base = (await import(`../../messages/${locale}.json`)).default;
-  const cv = (await import(`../../messages/cv/${locale}.json`)).default;
+  const cv = (await import(`../../messages/cv/${locale}.json`)).default;  
   const projects = (await import(`../../messages/projects/${locale}.json`)).default;
   const contact = (await import(`../../messages/contact/${locale}.json`)).default;
+  
 
   return {
     locale,
